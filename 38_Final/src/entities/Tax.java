9eb5efeea5b23227d2a0890297452453ebdf66d6
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-04 11:43:58 +0100

    Oprettet Tax

---
+++ 38_Final/src/entities/Tax.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Tax extends Felt {
+
+	public Tax(String feltNavn) {
+		super(feltNavn);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void landOnField(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFeltBesked(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
