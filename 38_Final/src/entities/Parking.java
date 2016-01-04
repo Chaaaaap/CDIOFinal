ef051f2025aa1eac6fbe407c5f5126db100b81a5
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-04 11:44:37 +0100

    Oprettet Parking

---
+++ 38_Final/src/entities/Parking.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Parking extends Felt {
+
+	public Parking(String feltNavn) {
+		super(feltNavn);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void landOnField(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFeltBesked(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
