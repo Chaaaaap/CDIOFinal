feed988f7f7f211e7f19f0cebedf380c5d3c3110
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-04 11:43:02 +0100

    Oprettet Shipping Company felt

---
+++ 38_Final/src/entities/ShippingCompany.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class ShippingCompany extends Ownable {
+
+	public ShippingCompany(int pris, String feltNavn) {
+		super(pris, feltNavn);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getRent(Player player) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setOwner(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Player getOwner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void buyFieldOption(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFeltBesked(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
