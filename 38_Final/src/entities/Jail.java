43dbc02466df96f592cbd0e38fea3225555fe094
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-04 11:40:19 +0100

    Tilføjet Jail felt

---
+++ 38_Final/src/entities/Jail.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Jail extends Felt {
+
+	public Jail(String feltNavn) {
+		super(feltNavn);
+	}
+
+	@Override
+	public void landOnField(Player player) {
+
+	}
+
+	@Override
+	public String getFeltBesked(Player player) {
+		return null;
+	}
+
+}
