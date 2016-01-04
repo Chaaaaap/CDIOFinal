3dd9b7950d3ccdf92f584a0e5ecfeac4d5fef325
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-04 11:39:08 +0100

    Tilføjet GoToJail felt

---
+++ 38_Final/src/entities/GoToJail.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class GoToJail extends Felt {
+
+	public GoToJail(String feltNavn) {
+		super(feltNavn);
+	}
+
+	@Override
+	public void landOnField(Player player) {
+
+	}
+
+	@Override
+	public String getFeltBesked(Player player) {
+		return null;
+	}
+
+}
