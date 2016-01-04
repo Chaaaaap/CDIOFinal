53fa5032fa0163580c592eab3cbdf7c07526d996
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-04 11:36:27 +0100

    Har bare rykket en linje lidt ind

--- 38_Final/src/entities/Felt.java
+++ 38_Final/src/entities/Felt.java
@@ -1,16 +1,16 @@
 package entities;
 
 public abstract class Felt {
-	
-private String feltNavn;
-	
+
+	private String feltNavn;
+
 	public Felt(String feltNavn) {
 		this.feltNavn = feltNavn;
 	}
-	
+
 	//These methods are abstract because we don't ever use them from the Felt class.
 	//They will always be overriden from the subclasses of Felt.
 	public abstract void landOnField(Player player);
 	public abstract String getFeltBesked(Player player);
-	
+
 }
