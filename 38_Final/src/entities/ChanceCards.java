39d88e2c97ee66e083aef20a42f7073e357d1c8c
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-05 13:40:03 +0100

    Oprettet String til alle vores kort

--- 38_Final/src/entities/ChanceCards.java
+++ 38_Final/src/entities/ChanceCards.java
@@ -2,4 +2,41 @@ package entities;
 
 public class ChanceCards {
 
+	String[] chanceCards = new String[] {
+	"ChanceFerry",
+	"ChanceJail",
+	"ChancePayFine",
+	"ChancePayInsurence",
+	"ChanceShip",
+	"ChanceAktie",
+	"ChanceBirthday",
+	"ChanceFine",
+	"ChanceTown",
+	"ChanceBond",
+	"ChanceKing",
+	"ChanceOil",
+	"ChanceMove",
+	"ChanceLottery",
+	"ChanceRepair",
+	"ChanceTax",
+	"ChanceShippingCompany",
+	"ChanceGrønningen",
+	"ChancePropertyTax",
+	"ChanceTipning",
+	"ChanceStart",
+	"ChanceMatador",
+	"ChanceGarden",
+	"ChanceDentist",
+	"ChanceAbroad",
+	"ChanceFrederiksberg",
+	"ChanceDyrtid",
+	"ChanceAktie",
+	"ChanceAktie",
+	"ChanceAktie",
+	"ChanceAktie",
+	"ChanceKing",
+	"ChanceRepair"
+	};
+
+
 }
