bfdfd49595d5e23d762a56d4512dcbe622d622d8
Silas El-Azm Stryhn <s143599@student.dtu.dk>
2016-01-06 14:51:14 +0100

    Igang med at lave Tax og ændret lidt i tekstfiler

--- 38_Final/src/entities/Tax.java
+++ 38_Final/src/entities/Tax.java
@@ -1,30 +1,50 @@
 package entities;
 
 import java.util.ResourceBundle;
+import desktop_resources.GUI;
+import entities.Player;
 
 public class Tax extends Felt {
 	
-	private int rent;
+	private String Tax, feltNavn;
+	private int Pay, taxAmount;
+	private Player player;
+	private ResourceBundle rb;
+//	private String[] strings;
 
-	public Tax(int rent, String feltNavn, ResourceBundle rb) {
+	public Tax(int tax, String feltNavn, ResourceBundle rb) {
 		super(feltNavn);
-		this.rent = rent;
+		this.feltNavn = feltNavn;
+		taxAmount = tax;
 	}
 
 	@Override
 	public void landOnField(Player player) {
-		getFeltBesked(player);
-		getRent(player);
+		this.player = player;
+		GUI.showMessage(getFeltBesked(player));
+		payTax();
 		
+	}
+
+	private void payTax() {
+		Tax = GUI.getUserButtonPressed("Choose between paying "+taxAmount+" or 10% of your assets.", "Pay "+taxAmount, "Pay "+tenPercent(player));
+		Pay = Integer.parseInt(Tax.replaceAll("[\\D]", ""));
+		player.getPlayerAccount().adjustBalance(-Pay);
+		GUI.setBalance(player.getPlayerName(), player.getPlayerAccount().getBalance());
+		
+	}
+
+	private int tenPercent(Player player) {
+		return player.getPlayerAccount().getBalance()/10;
 	}
 
 	@Override
 	public String getFeltBesked(Player player) {
-		return null;
+		return player.getPlayerName()+", "+rb.getString("Rent")+" "+feltNavn+".";
 	}
 	
-	private int getRent(Player player) {
-		return rent;
-	}
+//	private int getRent(Player player) {
+//		return rent;
+//	}
 
 }
