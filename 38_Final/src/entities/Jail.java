6bc974db5d4e4ae6c85e830884be153fb3cd2500
Silas El-Azm Stryhn <s143599@student.dtu.dk>
2016-01-06 15:30:58 +0100

    Nu kommer der besked når du er på besøg i fængslet

--- 38_Final/src/entities/Jail.java
+++ 38_Final/src/entities/Jail.java
@@ -3,21 +3,25 @@ package entities;
 import java.util.ResourceBundle;
 
 import controllers.GameBoard;
+import desktop_resources.GUI;
 
 public class Jail extends Felt {
+	
+	private ResourceBundle rb;
 
 	public Jail(String feltNavn, GameBoard gameBoard, ResourceBundle rb) {
 		super(feltNavn);
+		this.rb =rb;
 	}
 
 	@Override
 	public void landOnField(Player player) {
-
+		GUI.showMessage(getFeltBesked(player));
 	}
 
 	@Override
 	public String getFeltBesked(Player player) {
-		return null;
+		return player.getPlayerName()+", "+rb.getString("Jail");
 	}
 
 }
