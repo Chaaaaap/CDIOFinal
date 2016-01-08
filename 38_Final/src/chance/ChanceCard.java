1cb1753b99ed52e6079cdeaf72796b516a5d685f
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-08 11:57:07 +0100

    Flyttet metoder fra ChanceCard til Controlleren

--- 38_Final/src/chance/ChanceCard.java
+++ 38_Final/src/chance/ChanceCard.java
@@ -14,23 +14,23 @@ public abstract class ChanceCard {
 		this.cardName = cardName;
 	}
 	
-	public void shuffle() {
-		for(int i = 0; i < chanceCards.length; i++) {
-			int j = generator.nextInt(chanceCards.length);
-			ChanceCard tmp = chanceCards[i];
-			chanceCards[i] = chanceCards[j];
-			chanceCards[j] = tmp;
-		}
-	}
+//	public void shuffle() {
+//		for(int i = 0; i < chanceCards.length; i++) {
+//			int j = generator.nextInt(chanceCards.length);
+//			ChanceCard tmp = chanceCards[i];
+//			chanceCards[i] = chanceCards[j];
+//			chanceCards[j] = tmp;
+//		}
+//	}
 	
-	public ChanceCard drawCard() {
-		ChanceCard drawn;
-		drawn = chanceCards[0];
-		for(int i=0;i<51;i++){
-			chanceCards[i]=chanceCards[i+1];
-		}
-		return drawn;
-	}
+//	public ChanceCard drawCard() {
+//		ChanceCard drawn;
+//		drawn = chanceCards[0];
+//		for(int i=0;i<51;i++){
+//			chanceCards[i]=chanceCards[i+1];
+//		}
+//		return drawn;
+//	}
 	
 //	public String[] getCards() {
 //		return chanceCards;
--- 38_Final/src/controllers/ChanceCardController.java
+++ 38_Final/src/controllers/ChanceCardController.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import java.util.Random;
+
 import chance.ChanceCard;
 
 public class ChanceCardController {
 
 	private ChanceCard[] chanceCards;
+	private Random generator;
 
 	public ChanceCard drawCard() {
 		ChanceCard drawn;
@@ -13,6 +16,15 @@ public class ChanceCardController {
 			chanceCards[i]=chanceCards[i+1];
 		}
 		return drawn;
+	}
+	
+	public void shuffle() {
+		for(int i = 0; i < chanceCards.length; i++) {
+			int j = generator.nextInt(chanceCards.length);
+			ChanceCard tmp = chanceCards[i];
+			chanceCards[i] = chanceCards[j];
+			chanceCards[j] = tmp;
+		}
 	}
 
 }
