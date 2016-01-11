6a6333678eba142b0ed3d80b6c641acf4f293a1f
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-11 12:49:14 +0100

    Nu virker ChanceMove måske helt

--- 38_Final/src/chance/ChanceMove.java
+++ 38_Final/src/chance/ChanceMove.java
@@ -1,29 +1,46 @@
 package chance;
 
+import java.util.ResourceBundle;
+
+import controllers.ChanceCardController;
 import controllers.GameBoard;
 import desktop_resources.GUI;
+import entities.ChanceField;
+import entities.Felt;
 import entities.Player;
 
 public class ChanceMove extends ChanceCard {
 
 	private int antalFelter;
 	private GameBoard gb;
+	private ChanceCard[] cc;
+	private ChanceCard chanceCard;
+	private ResourceBundle rb;
+	private ChanceCardController ccc;
 
-	public ChanceMove(String cardName, int antalFelter, GameBoard gameBoard) {
+	public ChanceMove(String cardName, int antalFelter, GameBoard gameBoard, ChanceCard[] cc, ResourceBundle rb, ChanceCardController ccc) {
 		super(cardName);
 		this.antalFelter = antalFelter;
 		this.gb = gameBoard;
+		this.cc = cc;
+		this.rb = rb;
+		this.ccc = ccc;
 	}
 
 	@Override
 	public void executeCard(Player player) {
 		GUI.removeAllCars(player.getPlayerName());
-		
+		player.setCurrentField(player.getCurrentField()+antalFelter);
+		Felt currentField = gb.getlogicFields()[player.getCurrentField()];
 		if(player.getCurrentField() < 4) {
 			player.setCurrentField(39);
 			GUI.setCar(player.getCurrentField()+1, player.getPlayerName());
+		} else if(currentField instanceof ChanceField) {
+			GUI.showMessage(rb.getString(gb.getlogicFields()[player.getCurrentField()].getFeltBesked(player)));
+			chanceCard = ccc.drawCard();
+			GUI.showMessage(rb.getString(chanceCard.toString()));
+			chanceCard.executeCard(player);
 		} else 	{
-			player.setCurrentField(player.getCurrentField()+antalFelter);
 			GUI.setCar(player.getCurrentField()+1, player.getPlayerName());
 		}
 
