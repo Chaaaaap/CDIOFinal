885c0b55bada44078b583ca92b01867255e90898
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-14 14:57:24 +0100

    Cleanup

--- 38_Final/src/entities/Felt.java
+++ 38_Final/src/entities/Felt.java
@@ -2,10 +2,7 @@ package entities;
 
 public abstract class Felt {
 
-	private String feltNavn;
-
 	public Felt(String feltNavn) {
-		this.feltNavn = feltNavn;
 	}
 
 	//These methods are abstract because we don't ever use them from the Felt class.
