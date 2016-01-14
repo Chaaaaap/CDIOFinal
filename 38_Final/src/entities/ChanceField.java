54a8537d3183213a4fd924fb0d22c32fa3019da3
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-01-14 14:59:17 +0100

    Ret meget cleanup

--- 38_Final/src/entities/ChanceField.java
+++ 38_Final/src/entities/ChanceField.java
@@ -1,39 +1,13 @@
 package entities;
 
-import java.util.ResourceBundle;
-
-import chance.ChanceCard;
-import controllers.GameBoard;
-import desktop_resources.GUI;
-
 public class ChanceField extends Felt {
 
-	private ChanceCard[] chanceCards;
-	private ChanceCard chanceCard;
-	private String cardText;
-	private ChanceCard cc;
-	private ResourceBundle rb;
-
-	public ChanceField(String feltNavn, GameBoard gameBoard, ResourceBundle rb) {
+	public ChanceField(String feltNavn) {
 		super(feltNavn);
-		this.rb = rb;
-		chanceCards = new ChanceCard[33];
-		initCards();
-//		chanceCard.shuffle();
-		
-	}
-
-	private void initCards() {
-		
-		
 	}
 
 	@Override
-	public void landOnField(Player player) {
-//		getFeltBesked(player);
-//		chanceCard = cc.drawCard();
-//		GUI.showMessage(rb.getString(chanceCard.toString()));
-//		chanceCard.executeCard(player);		
+	public void landOnField(Player player) {	
 	}
 
 	@Override
