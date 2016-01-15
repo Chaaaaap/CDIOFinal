26fa028c694493d4caa6c5b1b12d81992ee0a688
Martin <Martin@10.16.98.38>
2016-01-15 13:20:51 +0100

    TestJail er færdig og virker

--- 38_Final/src/test/TestJail.java
+++ 38_Final/src/test/TestJail.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import controllers.*;
+import desktop_resources.GUI;
 import entities.*;
 
 
@@ -22,7 +23,7 @@ public class TestJail {
 	public void setUp() throws Exception {
 		diceOne = new Dice(1,1);
 		diceTwo = new Dice(1,1);
-		diceCup = new DiceCup();
+		diceCup = new DiceCup(diceOne,diceTwo);
 		player = new Player();
 		player.getPlayerAccount().setBalance(30000);
 		player.setPlayerName("Player");	
@@ -30,20 +31,27 @@ public class TestJail {
 
 	@Test
 	public void test() {
-		player.addTurnCounter();
-		diceCup.shake();
-//		
-//		int expected = diceCup.getDiceOne();
-//		int expected1 = diceCup.getDiceTwo();
-//		
-//		assertEquals(expected, expected1);
 		
-		diceCup.shake();
-		diceCup.shake();
-		
-		
-		boolean expected2 = true;
+		boolean expected2 = false;
 		boolean actual = player.isJailed;
+		assertEquals(expected2, actual);
+		
+		for (int i = 0; i < 3; i++) {
+			diceCup.shake();
+			player.addTurnCounter();
+		}
+		
+		int expected = diceCup.getDiceOne();
+		int expected1 = diceCup.getDiceTwo();
+		assertEquals(expected, expected1);
+		
+	while(diceCup.getDiceOne() == diceCup.getDiceTwo() && player.getTurnCounter() < 3);
+		if(player.getTurnCounter() == 3) {
+			player.isJailed = true;
+		}
+		
+		expected2 = true;
+		actual = player.isJailed;
 		assertEquals(expected2, actual);
 	}
 
