a50a424d05db9022d6bfc2e420186a525bc7fb75
IamHexor <ykhexor@hotmail.com>
2016-01-15 11:42:15 +0100

    Test af chancekort ryk 3 felter tilbage er lavet

---
+++ 38_Final/src/test/TestMoveThreeFieldsBack.java
@@ -0,0 +1,64 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ResourceBundle;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import controllers.*;
+import chance.*;
+import entities.*;
+
+public class TestMoveThreeFieldsBack {
+
+	private Player player;
+	private GameBoard gb;
+	private DiceCup diceCup;
+	private ResourceBundle rb;
+	private LanguageSelector ls;
+
+	private ChanceCard[] cc;
+	private ChanceCardController chanceCardController;
+
+	private ChanceField PrøvLykken;
+
+
+	@Before
+	public void setUp() throws Exception {
+		ls = new LanguageSelector("da", "DK");
+		gb = new GameBoard(diceCup, ls);
+		rb = ls.selectLanguage("da", "DK");
+		player = new Player();
+		player.getPlayerAccount().setBalance(30000);
+		player.setPlayerName("Player");
+
+		cc = new ChanceCard[1];
+		chanceCardController = new ChanceCardController(cc);
+		cc[0] = new ChanceMove("ChanceMove", -3, gb, chanceCardController, rb);
+
+		PrøvLykken = new ChanceField("Prøv Lykken");
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBuyHouse() {
+
+
+		player.setCurrentField(2);
+		PrøvLykken.landOnField(player);
+		cc[0].executeCard(player);
+
+		int expected = 39;
+		int actual = player.getCurrentField();		
+
+		assertEquals(expected, actual);
+	}
+
+}
