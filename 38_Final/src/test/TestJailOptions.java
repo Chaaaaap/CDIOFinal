f70dcd41900af69f59452cee86a9b127c5b68803
Martin <Martin@10.16.98.38>
2016-01-15 13:29:29 +0100

    TestJailedOption virker nu

--- 38_Final/src/test/TestJailOptions.java
+++ 38_Final/src/test/TestJailOptions.java
@@ -23,9 +23,13 @@ public class TestJailOptions {
 	private GoToJail gtj;
 	private String jailedOption;
 	private int sum;
+	private Dice diceOne, diceTwo;
 
 	@Before
 	public void setUp() throws Exception {
+		diceOne = new Dice(1,1);
+		diceTwo = new Dice(1,1);
+		diceCup = new DiceCup(diceOne,diceTwo);
 		ls = new LanguageSelector("da", "DK");
 		gb = new GameBoard(diceCup, ls);
 		rb = ls.selectLanguage("da", "DK");
@@ -51,13 +55,13 @@ public class TestJailOptions {
 
 	@Test
 	public void testJailOptionPay() {
-		
+
 		player.isJailed = true;
-		
+
 		int expected1 = 30000;
 		int actual1 = player.getBalance(player);
 		assertEquals(expected1, actual1);
-		
+
 		jailedOption = GUI.getUserButtonPressed(player.getPlayerName()+", "+rb.getString("Jail3"), rb.getString("Jail5"));
 
 		if(jailedOption.equals(rb.getString("Jail5"))) {
@@ -65,22 +69,23 @@ public class TestJailOptions {
 			GUI.setBalance(player.getPlayerName(), player.getPlayerAccount().getBalance());
 			player.isJailed = false;
 		}
-		
+
 		boolean expected = false;
 		boolean actual = player.isJailed;
 		assertEquals(expected, actual);
-		
+
 		expected1 = 29000;
 		actual1 = player.getBalance(player);
 		assertEquals(expected1, actual1);
 
 	}
+	@Test
 	public void testJailOptionDiceRoll() {
-		
+
 		player.isJailed = true;
-		
+
 		jailedOption = GUI.getUserButtonPressed(player.getPlayerName()+", "+rb.getString("Jail3"), rb.getString("Jail6"));
-		
+
 		if(jailedOption.equals(rb.getString("Jail6"))){
 			diceCup.shake();
 			GUI.setDice(diceCup.getDiceOne(), diceCup.getDiceTwo());
@@ -88,34 +93,29 @@ public class TestJailOptions {
 			if(diceCup.getDiceOne() == diceCup.getDiceTwo()){
 				GUI.showMessage(player.getPlayerName()+", "+rb.getString("Jail8"));
 				player.isJailed = false;
-				player.setJailRoll(0);		
-				sum = diceCup.getSumResult();
-				GUI.setDice(diceCup.getDiceOne(), diceCup.getDiceTwo());
-				GUI.removeAllCars(player.getPlayerName());
-				player.setCurrentField(player.getCurrentField()+sum);
-				GUI.setCar(player.getCurrentField()+1, player.getPlayerName());
-				gb.getlogicFields()[player.getCurrentField()].landOnField(player);
-			}
-
-			else GUI.showMessage(player.getPlayerName()+", "+rb.getString("Jail9")); {
-				if (player.getJailRoll() == 2) {
-					GUI.showMessage(player.getPlayerName()+", "+rb.getString("Jail10"));
-					player.adjustBalance(player, -1000);
-					GUI.setBalance(player.getPlayerName(), player.getBalance(player));
-					player.isJailed = false;
-					player.setJailRoll(0);
-					GUI.setDice(diceCup.getDiceOne(), diceCup.getDiceTwo());
-				}
-
-				else {
-					player.addJailRollCounter();
-					GUI.setDice(diceCup.getDiceOne(), diceCup.getDiceTwo());
-				}
 			}
 		}
-		
-		
-		
-		
+		boolean expected = false;
+		boolean actual = player.isJailed;
+		assertEquals(expected, actual);
 	}
+
+	@Test
+	public void testJailOptionFreeCard() {
+
+		player.isJailed = true;
+		player.addFreeCard();
+
+		jailedOption = GUI.getUserButtonPressed(player.getPlayerName()+", "+rb.getString("Jail3"), rb.getString("Jail7"));
+
+		if(jailedOption.equals(rb.getString("Jail7"))) {
+			player.useFreeCard();
+			player.isJailed = false;
+		}
+
+		boolean expected = false;
+		boolean actual = player.isJailed;
+		assertEquals(expected, actual);
+	}
+
 }
